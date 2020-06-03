84b910fc8cbcc27192621e1b889321706e07cbf3
paulwongx <20178251+paulwongx@users.noreply.github.com>
2020-06-03 19:47:17 -0400

    Updates

---
+++ precache-manifest.07b39bbae2ccaeefde02d41e5f45d016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd3e948548b4b1a764ae1d5fa9e42a7",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "cdae4aa893309820612c",
+    "url": "/create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "827ff8125628448b0af6",
+    "url": "/create-react-app/static/js/2.be032748.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.be032748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdae4aa893309820612c",
+    "url": "/create-react-app/static/js/main.7d030255.chunk.js"
+  },
+  {
+    "revision": "0658783eab913069a4fe",
+    "url": "/create-react-app/static/js/runtime-main.d68f218a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
